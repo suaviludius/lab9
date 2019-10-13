bf6c2ba4c75315bec89d44b40c1d177ae79a218a
suaviludius <47569681+suaviludius@users.noreply.github.com>
2019-10-13 22:23:11 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,19 +1,35 @@
 #include <conio.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #define N 180
 
-//количество букв, слов и предложений в тексте
+//стАТИСТИКА ПО ФАЙЛУ(СИМВОЛЫ)
 
-int main()
+int main(int argc, char* argv [])
 {
 	FILE* f;
 	int simv = 0; //Счетчик колличества одинаковых символов 
 	char arr[N]; // Массив символов файла
 	int rat[N] = { 0 }; // Массив колличества повторений символов
 	int k = 0; //количество символов в файле
-	
-	if ((f = fopen( "D:/text.txt", "r")) == NULL) //Открытие файла для чтения
+	char filename[100] = { 0 };
+
+	//for (int count = 0; count < argc; ++count)
+		//printf(" %s\n", argv[count]);
+
+	printf("Please enter a file address or enter a number from 1 to 3 to open the default file: ");
+	gets_s(filename);
+	if (filename[0] == 0)
+	{
+		printf("\bIf there is no input value, the default file will be taken.\n");
+		printf("Please enter a file address: ");
+		gets_s(filename);
+	}
+
+	srand(time(NULL));
+	if ((f = fopen((filename[0] == 0 ? argv[rand() % 3 + 1] :filename[0] == '1' ? argv[1] :filename[0] == '2' ? argv[2] : filename[0] == '3' ? argv[3] : filename), "r")) == NULL) //Открытие файла для чтения
 	{
 		printf("Cannot open input file.\n");
 		return 1;
@@ -38,10 +54,10 @@ int main()
 		{
 			if (ch == arr[i]) simv++; 
 		}
-		if (ch == '\n') printf("Symbol  \\n: %3i time\n", simv); else // Новая строка
-			if (ch == '\t') printf("Symbol  \\t: %3i time\n", simv); else // Табуляция
-				if (ch == -1) printf("Symbol EOF: %3i time\n", simv); else //Символ конца файла
-					printf("Symbol %3c: %3i time\n", ch, simv); // Символ и его количество
+		if (ch == '\n') printf("Symbol  \\n: %3i/%i time \n ", simv, k); else // Новая строка
+			if (ch == '\t') printf("Symbol  \\t: %3i/%i  time\n", simv, k); else // Табуляция
+				if (ch == -1) printf("Symbol EOF: %3i/%i time\n", simv,k); else //Символ конца файла
+					printf("Symbol %3c: %3i/%i time\n", ch, simv, k); // Символ и его количество
 		rat[t] = simv; //Запоминаем колличество символов, встретившихся впервые, с номером их расположения в arr[]
 	}
 	printf("\nCharacter output by frequency: \n"); //"Вывод символов по частоте"
@@ -72,3 +88,4 @@ int main()
 	fclose(f);
 	return 0;
 }
+
