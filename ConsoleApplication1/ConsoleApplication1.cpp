1d531a37e7d593c42cf34b299815dd9fe4764515
suaviludius <47569681+suaviludius@users.noreply.github.com>
2019-10-13 22:50:56 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,7 +7,7 @@
 
 //стАТИСТИКА ПО ФАЙЛУ(СИМВОЛЫ)
 
-int main(int argc, char* argv [])
+int main(int argc, char* argv[])
 {
 	FILE* f;
 	int simv = 0; //Счетчик колличества одинаковых символов 
@@ -29,7 +29,7 @@ int main(int argc, char* argv [])
 	}
 
 	srand(time(NULL));
-	if ((f = fopen((filename[0] == 0 ? argv[rand() % 3 + 1] :filename[0] == '1' ? argv[1] :filename[0] == '2' ? argv[2] : filename[0] == '3' ? argv[3] : filename), "r")) == NULL) //Открытие файла для чтения
+	if ((f = fopen((filename[0] == 0 ? argv[rand() % 3 + 1] : filename[0] == '1' ? argv[1] : filename[0] == '2' ? argv[2] : filename[0] == '3' ? argv[3] : filename), "r")) == NULL) //Открытие файла для чтения
 	{
 		printf("Cannot open input file.\n");
 		return 1;
@@ -38,54 +38,57 @@ int main(int argc, char* argv [])
 	while ((arr[k] = fgetc(f)) != EOF) //Заносим символы файла в массив
 		k++;
 
-	printf("Statistics by text file: \n"); //"Статистика по текстовому файлу"
-	for (int t = 0; t < k+1; t++, simv = 0) //Просмотр символов массива символов
-	{
-		char ch = arr[t]; //символ для сравнения
-		for (int i = 0; i < t+1; i++)// Элементы до символа
+	FILE* fpout;
+	fpout = fopen("D:\\Statistic\\Statistic.txt", "w");
+		fprintf(fpout,"Statistics by text file: \n"); //"Статистика по текстовому файлу"
+		for (int t = 0; t < k + 1; t++, simv = 0) //Просмотр символов массива символов
 		{
-			if (ch == arr[i])
+			char ch = arr[t]; //символ для сравнения
+			for (int i = 0; i < t + 1; i++)// Элементы до символа
 			{
-				simv++;
+				if (ch == arr[i])
+				{
+					simv++;
+				}
 			}
+			if (simv > 1) continue;
+			for (int i = t + 1; i < k; i++) // Элементы после символа
+			{
+				if (ch == arr[i]) simv++;
+			}
+			if (ch == '\n') fprintf(fpout, "Symbol  \\n: %3i/%i time \n ", simv, k); else // Новая строка
+				if (ch == '\t') fprintf(fpout, "Symbol  \\t: %3i/%i  time\n", simv, k); else // Табуляция
+					if (ch == -1) fprintf(fpout, "Symbol EOF: %3i/%i time\n", simv, k); else //Символ конца файла
+						fprintf(fpout, "Symbol %3c: %3i/%i time\n", ch, simv, k); // Символ и его количество
+			rat[t] = simv; //Запоминаем колличество символов, встретившихся впервые, с номером их расположения в arr[]
 		}
-		if (simv > 1) continue;
-		for (int i = t+1; i < k; i++) // Элементы после символа
-		{
-			if (ch == arr[i]) simv++; 
-		}
-		if (ch == '\n') printf("Symbol  \\n: %3i/%i time \n ", simv, k); else // Новая строка
-			if (ch == '\t') printf("Symbol  \\t: %3i/%i  time\n", simv, k); else // Табуляция
-				if (ch == -1) printf("Symbol EOF: %3i/%i time\n", simv,k); else //Символ конца файла
-					printf("Symbol %3c: %3i/%i time\n", ch, simv, k); // Символ и его количество
-		rat[t] = simv; //Запоминаем колличество символов, встретившихся впервые, с номером их расположения в arr[]
-	}
-	printf("\nCharacter output by frequency: \n"); //"Вывод символов по частоте"
+	fclose(fpout);
+
+	printf("Character output by frequency: \n"); //"Вывод символов по частоте"
 	for (int i = k; i > -1; i--)
 	{
-			if (rat[i] > 0)
+		if (rat[i] > 0)
+		{
+			for (int t = 0; t < i; t++)
 			{
-				for (int t = 0; t < i; t++)
+				if (rat[i] < rat[t])
 				{
-					if (rat[i] < rat[t])
-					{
-						int rat_swap = rat[i]; //своп
-						rat[i] = rat[t];
-						rat[t] = rat_swap;
+					int rat_swap = rat[i]; //своп
+					rat[i] = rat[t];
+					rat[t] = rat_swap;
 
-						char arr_swap= arr[t]; //пинг
-						arr[t] = arr[i];
-						arr[i] = arr_swap;
-					}
+					char arr_swap = arr[t]; //пинг
+					arr[t] = arr[i];
+					arr[i] = arr_swap;
 				}
-				if (arr[i] == '\n') printf("\\n"); else
-					if (arr[i] == '\t') printf("\\t"); else
-						if (arr[i] == -1) printf("EOF"); else
-				printf("%c", arr[i]);
 			}
+			if (arr[i] == '\n') printf("\\n"); else
+				if (arr[i] == '\t') printf("\\t"); else
+					if (arr[i] == -1) printf("EOF"); else
+						printf("%c", arr[i]);
+		}
 	}
 	printf("\n");
 	fclose(f);
 	return 0;
 }
-
